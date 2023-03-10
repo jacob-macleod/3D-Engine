bfa6169652e74bec906c27c15f64316f0b0bd926
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2023-03-10 12:41:49 +0000

    Added buttons for vertical movement

--- static/js/render.js
+++ static/js/render.js
@@ -14,41 +14,41 @@ function find_point(focal_length, point, z) {
 // This seems like a horribly inefficient way to do define it, so I need to research alternatives
 cube = [
     //x, y, z
-    "6,2,2",   // Front bottom-left corner
-    "9,2,2",   // Front bottom-right corner
+    "60,20,20",   // Front bottom-left corner
+    "90,20,20",   // Front bottom-right corner
 
-    "9,6,2",   // Front top-right corner
-    "8,6,2",   // Front top-left corner
+    "90,60,20",   // Front top-right corner
+    "80,60,20",   // Front top-left corner
 
-    "9,6,2",   // Front top-right corner
-    "9,2,2",   // Front bottom-right corner
+    "90,60,20",   // Front top-right corner
+    "90,20,20",   // Front bottom-right corner
 
-    "6,6,2",   // Front top-left corner
-    "6,2,2",   // Front bottom-left corner
+    "60,60,20",   // Front top-left corner
+    "60,20,20",   // Front bottom-left corner
 
-    "6,2,6",   // Back bottom-left corner
-    "9,2,6",   // Back bottom-right corner
+    "60,20,60",   // Back bottom-left corner
+    "90,20,60",   // Back bottom-right corner
 
-    "9,6,6",   // Back top-right corner
-    "6,6,6",   // Back top-left corner
+    "90,60,60",   // Back top-right corner
+    "60,60,60",   // Back top-left corner
 
-    "9,6,6",   // Back top-right corner
-    "9,2,6",   // Back bottom-right corner
+    "90,60,60",   // Back top-right corner
+    "90,20,60",   // Back bottom-right corner
 
-    "6,6,6",   // Back top-left corner
-    "6,2,6",   // Back bottom-left corner
+    "60,60,60",   // Back top-left corner
+    "60,20,60",   // Back bottom-left corner
 
-    "9,6,2",   // Front top-right corner
-    "9,6,6",   // Back top-right corner
+    "90,60,20",   // Front top-right corner
+    "90,60,60",   // Back top-right corner
 
-    "6,6,2",   // Front top-left corner
-    "6,6,6",   // Back top-left corner
+    "60,60,20",   // Front top-left corner
+    "60,60,60",   // Back top-left corner
 
-    "6,2,2",   // Front bottom-left corner
-    "6,2,6",   // Back bottom-left corner
+    "60,20,20",   // Front bottom-left corner
+    "60,20,60",   // Back bottom-left corner
 
-    "9,2,2",   // Front bottom-right corner
-    "9,2,6",   // Back bottom-right corner
+    "90,20,20",   // Front bottom-right corner
+    "90,20,60",   // Back bottom-right corner
 ]
 
 function render_cube() {
@@ -58,14 +58,14 @@ function render_cube() {
         if ((i%2) == 0) {
             // Draw the line
             coords = cube[i].split(",")
-            x1 = parseInt(coords[0]) * 100
-            y1 = parseInt(coords[1]) * 100
-            z1 = parseInt(coords[2]) * 100
+            x1 = parseInt(coords[0])
+            y1 = parseInt(coords[1])
+            z1 = parseInt(coords[2])
 
             next_coords = cube[i+1].split(",")
-            x2 = parseInt(next_coords[0]) * 100
-            y2 = parseInt(next_coords[1]) * 100
-            z2 = parseInt(next_coords[2]) * 100
+            x2 = parseInt(next_coords[0])
+            y2 = parseInt(next_coords[1])
+            z2 = parseInt(next_coords[2])
 
             x1 = find_point(focal_length, x1, z1)
             y1 = find_point(focal_length, y1, z1)
@@ -75,7 +75,6 @@ function render_cube() {
             ctx.moveTo(x1, y1)
             ctx.lineTo(x2, y2);
             ctx.stroke();
-            console.log(x1)
         }
     }
 }
@@ -93,6 +92,32 @@ function move_horizontally(num_of_steps) {
         new_coordinate = parseInt(coords[0])
         new_coordinate = new_coordinate + num_of_steps
         coords[0] = (new_coordinate).toString()
+        line = coords[0] + "," + coords[1] + "," + coords[2]
+        newArr.push(line)
+    }
+
+    // Clear canvas
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+
+    cube = newArr
+
+    // Redraw the cube
+    render_cube()
+}
+
+// Increment all y coords in the array by num_of_steps
+function move_vertically(num_of_steps) {
+    newArr = []
+    coords = []
+
+    for (i=0;i<(cube.length); i++) {
+        coords = cube[i].split(",")
+        // Convert the x coord to a number, add num_of_steps to it, and turn it back into a string
+        new_coordinate = parseInt(coords[1])
+        new_coordinate = new_coordinate + num_of_steps
+        coords[1] = (new_coordinate).toString()
         line = coords[0] + "," + coords[1] + "," + coords[2]
         newArr.push(line)
     }
