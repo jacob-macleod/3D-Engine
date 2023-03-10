1fd331ed33e124f60e6aae5c76197f12cc3a4201
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2023-03-10 12:37:33 +0000

    Added function to move object horizontally

--- static/js/render.js
+++ static/js/render.js
@@ -51,29 +51,59 @@ cube = [
     "9,2,6",   // Back bottom-right corner
 ]
 
-// Draw the cube
-for (i=0;i<(cube.length); i++) {
-    // If i = 0, 2, 4, 6, etc
-    if ((i%2) == 0) {
-        // Draw the line
-        coords = cube[i].split(",")
-        x1 = parseInt(coords[0]) * 100
-        y1 = parseInt(coords[1]) * 100
-        z1 = parseInt(coords[2]) * 100
+function render_cube() {
+    // Draw the cube
+    for (i=0;i<(cube.length); i++) {
+        // If i = 0, 2, 4, 6, etc
+        if ((i%2) == 0) {
+            // Draw the line
+            coords = cube[i].split(",")
+            x1 = parseInt(coords[0]) * 100
+            y1 = parseInt(coords[1]) * 100
+            z1 = parseInt(coords[2]) * 100
 
-        next_coords = cube[i+1].split(",")
-        x2 = parseInt(next_coords[0]) * 100
-        y2 = parseInt(next_coords[1]) * 100
-        z2 = parseInt(next_coords[2]) * 100
+            next_coords = cube[i+1].split(",")
+            x2 = parseInt(next_coords[0]) * 100
+            y2 = parseInt(next_coords[1]) * 100
+            z2 = parseInt(next_coords[2]) * 100
 
-        x1 = find_point(focal_length, x1, z1)
-        y1 = find_point(focal_length, y1, z1)
-        x2 = find_point(focal_length, x2, z2)
-        y2 = find_point(focal_length, y2, z2)
+            x1 = find_point(focal_length, x1, z1)
+            y1 = find_point(focal_length, y1, z1)
+            x2 = find_point(focal_length, x2, z2)
+            y2 = find_point(focal_length, y2, z2)
 
-        ctx.moveTo(x1, y1)
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-        console.log(x1)
+            ctx.moveTo(x1, y1)
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+            console.log(x1)
+        }
     }
+}
+
+render_cube()
+
+// Increment all x coords in the array by num_of_steps
+function move_horizontally(num_of_steps) {
+    newArr = []
+    coords = []
+
+    for (i=0;i<(cube.length); i++) {
+        coords = cube[i].split(",")
+        // Convert the x coord to a number, add num_of_steps to it, and turn it back into a string
+        new_coordinate = parseInt(coords[0])
+        new_coordinate = new_coordinate + num_of_steps
+        coords[0] = (new_coordinate).toString()
+        line = coords[0] + "," + coords[1] + "," + coords[2]
+        newArr.push(line)
+    }
+
+    // Clear canvas
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+
+    cube = newArr
+
+    // Redraw the cube
+    render_cube()
 }
